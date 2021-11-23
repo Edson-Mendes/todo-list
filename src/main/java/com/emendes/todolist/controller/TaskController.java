8f973c901121394ba12c309f6208e12db7f595b0
Edson Mendes <edson.luiz.mendes@hotmail.com>
2021-11-23 08:54:13 -0300

    Antes de mudar método fromDTO

--- src/main/java/com/emendes/todolist/controller/TaskController.java
+++ src/main/java/com/emendes/todolist/controller/TaskController.java
@@ -1,51 +1,49 @@
 package com.emendes.todolist.controller;
 
-import java.util.List;
 
 import com.emendes.todolist.dto.TaskDto;
 import com.emendes.todolist.entities.Task;
 import com.emendes.todolist.services.TaskService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping()
+@Controller
+// @RequestMapping()
 public class TaskController {
-  
+
   @Autowired
   private TaskService taskService;
 
-  @GetMapping(value = "/tasks")
-  public ResponseEntity<List<TaskDto>> findAll(){
-    List<TaskDto> listTask = taskService.findAll();
-    return ResponseEntity.ok(listTask);
+  // Método para retornar a página de criação de tarefas
+  @RequestMapping(value = "/createTask", method = RequestMethod.GET)
+  public String form() {
+    return "task/taskForm";
   }
 
-  @PostMapping
-  @ResponseBody
-  public ResponseEntity<TaskDto> saveTask(@RequestBody TaskDto taskDto){
-    Task taskEntity = fromDto(taskDto);
-    taskService.saveTask(taskEntity);
-    return new ResponseEntity<TaskDto>(taskDto, HttpStatus.CREATED);
+  // Método para salvar uma tarefa.
+  @RequestMapping(value = "/createTask", method = RequestMethod.POST)
+  public String form(TaskDto taskDto) {
+    taskDto.setIsConcluded(false);
+    Task task = fromDto(taskDto);
+    taskService.saveTask(task);
+    return "redirect:/createTask";
   }
 
-  /* @PutMapping(path = "/{id}")
-  @ResponseBody
-  public ResponseEntity<TaskDto> updateTask(@PathVariable Integer id, @RequestBody TaskDto taskDto){
-    Task taskEntity = fromDto(taskDto);
+  @RequestMapping("/tasks")
+  public ModelAndView tasksList(){
+    ModelAndView modelAndView = new ModelAndView("index");
+    Iterable<TaskDto> tasks = taskService.findAll();
 
-  } */
+    modelAndView.addObject("tasks", tasks);
+    return modelAndView;
+  }
 
   // Método para converter uma taskdto em task
-  private Task fromDto(TaskDto taskDto){
+  private Task fromDto(TaskDto taskDto) {
     Task task = new Task(0, taskDto.getDescription(), taskDto.getIsConcluded());
     return task;
   }
